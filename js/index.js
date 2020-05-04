6f67c66808220ca7c15f4ef8c13ba40ef6542ee6
Dylan Lecomte <lecomtedylan@rocketmail.com>
2020-05-04 15:27:49 +0200

    you can now display the informations of your search

--- js/index.js
+++ js/index.js
@@ -1,25 +1,67 @@
 (function () {
     'use strict';
 
+    const LS_ID = "deez-web-app-dylan";
+    let favorites = [];
+
     const startSearch = $('#submitInput');
 
-    startSearch.click(function(e) {
+    startSearch.click(function (e) {
         e.preventDefault();
         search();
-    })
+    });
 
     function search() {
         const searchValue = $('#search').val();
         const sortValue = $('#sort').val();
-
+    
         console.log(searchValue, sortValue);
-
+    
         $.ajax({
             url: `https://api.deezer.com/search?q=${searchValue}&order=${sortValue}&output=jsonp`,
             dataType: 'jsonp'
-        }).then((result) => {
-            console.log(result);
         })
+    
+            .then((obj) => {
+    
+                const results = obj.data;
+    
+                console.log(results);
+    
+                let content = '';
+    
+                $.each(results, function (index, result) {
+                    content += `
+                <div class="result-ctn">
+                    <div class="img-add">
+                        <img src="${ result.album.cover}" alt="">
+                        <input type="submit" value="Ajouter aux favoris" id="${ result.id }" class="add">
+                    </div>
+                    <div class="result-infos">
+                        <h1>${ result.title}</h1>
+                        <h2>${ result.artist.name} / ${result.album.title}</h2>
+                        <audio controls src="${ result.preview}"></audio>
+                    </div>
+                </div>
+                `;
+                });
+    
+                $('.search-results').html(content);
+    
+                $('.add').click(function (e) {
+                    e.preventDefault();
+                    addFavorite($(this).attr('id'));
+                })
+            })
     }
-
+    
+    function addFavorite(id) {
+        console.log(id);
+        // favorites.push(song);
+        // localStorage.setItem(LS_ID, JSON.stringify(favorites));
+    }
+    
+    function deletefavorite() {
+    
+    }
 })();
