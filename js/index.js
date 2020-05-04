244ad3649fe5e099d7c81e96cd710380789a61b8
Dylan Lecomte <lecomtedylan@rocketmail.com>
2020-05-04 19:10:05 +0200

    you can now display your favorite songs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,48 @@
 (function () {
     'use strict';
 
-    const LS_ID = "deez-web-app-dylan";
+    const LOCALSTORAGE_ID = "deez-web-app-dylan";
     let favorites = [];
 
     const startSearch = $('#submitInput');
 
-    startSearch.click(function (e) {
-        e.preventDefault();
+    if (localStorage[LOCALSTORAGE_ID]) {
+        favorites.push(JSON.parse(localStorage[LOCALSTORAGE_ID]));
+        console.log('ARRAY FAVORITES', favorites);
+
+        for (let i = 0; i < favorites[0].length; i++) {
+
+            const song = favorites[0][i];
+
+            $('.favorites-ctn').append(
+                `
+        <div class="result-ctn">
+            <div class="img-add">
+                <img src="${song.album.cover}" alt="">
+                <input type="submit" value="Retirer des favoris" id="${song.id}">
+            </div>
+            <div class="song-infos">
+                <h1>${song.title}</h1>
+                <h2>${song.artist.name} / ${song.album.title}</h2>
+                <audio controls src="${song.preview}"></audio>
+            </div>
+        </div>
+        `);
+
+        $(`#${song.id}`).click(function (event) {
+            event.preventDefault();
+            deletefavorite(song);
+        })
+
+        }
+    } else {
+        $('.favorites-ctn').append(
+            `<h1>Aucun Favoris dans votre playliste actuellement...</h1>`);
+    }
+
+    startSearch.click(function (event) {
+        event.preventDefault();
+        $('.search-results').empty();
         search();
     });
 
@@ -15,18 +50,14 @@
         const searchValue = $('#search').val();
         const sortValue = $('#sort').val();
 
-        console.log(searchValue, sortValue);
-
         $.ajax({
             url: `https://api.deezer.com/search?q=${searchValue}&order=${sortValue}&output=jsonp`,
             dataType: 'jsonp'
         })
 
-            .then((obj) => {
+            .then(obj => {
 
                 const songs = obj.data;
-
-                console.log(songs);
 
                 $.each(songs, function (index, song) {
 
@@ -44,24 +75,60 @@
                     </div>
                 </div>
                 `);
-                
-                $(`#${song.id}`).click(function (e) {
-                    e.preventDefault();
-                    addFavorite(song);
-                })
+
+                    $(`#${song.id}`).click(function (event) {
+                        event.preventDefault();
+                        addFavorite(song);
+                    })
 
                 });
 
             })
+
+            .catch(error => {
+                if (error) throw error;
+            })
     }
 
     function addFavorite(song) {
-        console.log(song);
-        // favorites.push(song);
-        // localStorage.setItem(LS_ID, JSON.stringify(favorites));
+        console.log('SONG ADDED', song);
+        favorites.push(song);
+        localStorage.setItem(LOCALSTORAGE_ID, JSON.stringify(favorites));
+        console.log('LOCALSTORAGE', localStorage[LOCALSTORAGE_ID]);
+        console.log('ARRAY FAVORITES', favorites);
     }
 
     function deletefavorite(song) {
-
+        console.log(song);
     }
+
+    function displayFavorites() {
+        const ls = JSON.parse(localStorage[LOCALSTORAGE_ID]);
+        console.log(ls);
+        for (let i = 0; i < ls.length; i++) {
+            console.log('LSSSS', ls[i]);
+
+            $('.favorites-ctn').append(
+                `
+        <div class="result-ctn">
+            <div class="img-add">
+                <img src="${ls[i].album.cover}" alt="">
+                <input type="submit" value="Retirer des favoris" id="${ls[i].id}">
+            </div>
+            <div class="ls[i]-infos">
+                <h1>${ls[i].title}</h1>
+                <h2>${ls[i].artist.name} / ${ls[i].album.title}</h2>
+                <audio controls src="${ls[i].preview}"></audio>
+            </div>
+        </div>
+        `);
+
+            $(`#${ls[i].id}`).click(function (event) {
+                event.preventDefault();
+                deletefavorite(ls[i]);
+            })
+        }
+    }
+
+    //displayFavorites();
 })();
