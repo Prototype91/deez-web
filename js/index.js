bdab3d03e5fd3abfac4289cb57e32a5cd88d9ff5
Dylan Lecomte <lecomtedylan@rocketmail.com>
2020-05-04 16:38:33 +0200

    you can now trigger the song you want to add to your fav + search screen improved

--- js/index.js
+++ js/index.js
@@ -14,54 +14,54 @@
     function search() {
         const searchValue = $('#search').val();
         const sortValue = $('#sort').val();
-    
+
         console.log(searchValue, sortValue);
-    
+
         $.ajax({
             url: `https://api.deezer.com/search?q=${searchValue}&order=${sortValue}&output=jsonp`,
             dataType: 'jsonp'
         })
-    
+
             .then((obj) => {
-    
-                const results = obj.data;
-    
-                console.log(results);
-    
-                let content = '';
-    
-                $.each(results, function (index, result) {
-                    content += `
+
+                const songs = obj.data;
+
+                console.log(songs);
+
+                $.each(songs, function (index, song) {
+
+                    $('.search-results').append(
+                        `
                 <div class="result-ctn">
                     <div class="img-add">
-                        <img src="${ result.album.cover}" alt="">
-                        <input type="submit" value="Ajouter aux favoris" id="${ result.id }" class="add">
+                        <img src="${song.album.cover}" alt="">
+                        <input type="submit" value="Ajouter aux favoris" id="${song.id}">
                     </div>
-                    <div class="result-infos">
-                        <h1>${ result.title}</h1>
-                        <h2>${ result.artist.name} / ${result.album.title}</h2>
-                        <audio controls src="${ result.preview}"></audio>
+                    <div class="song-infos">
+                        <h1>${song.title}</h1>
+                        <h2>${song.artist.name} / ${song.album.title}</h2>
+                        <audio controls src="${song.preview}"></audio>
                     </div>
                 </div>
-                `;
-                });
-    
-                $('.search-results').html(content);
-    
-                $('.add').click(function (e) {
+                `);
+                
+                $(`#${song.id}`).click(function (e) {
                     e.preventDefault();
-                    addFavorite($(this).attr('id'));
+                    addFavorite(song);
                 })
+
+                });
+
             })
     }
-    
-    function addFavorite(id) {
-        console.log(id);
+
+    function addFavorite(song) {
+        console.log(song);
         // favorites.push(song);
         // localStorage.setItem(LS_ID, JSON.stringify(favorites));
     }
-    
-    function deletefavorite() {
-    
+
+    function deletefavorite(song) {
+
     }
 })();
