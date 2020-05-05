89a7af13854069cfd3ee7e42f45e03699eb08ccd
Dylan Lecomte <lecomtedylan@rocketmail.com>
2020-05-05 12:27:49 +0200

    js comments added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,40 @@
 (function () {
     'use strict';
 
+    //Localstorage and Sessionstorage ids
     const LOCALSTORAGE_ID = "deez-web-app-dylan";
+    const SESSIONSTORAGE_ID = "last-search-deez-web"
+
+    //Button to launch the search
     const startSearch = $('#submitInput');
+
+    //Array with all your fav songs
     let favorites = [];
 
-    function init() {
-        if (localStorage[LOCALSTORAGE_ID]) {
-            favorites = JSON.parse(localStorage[LOCALSTORAGE_ID]);
+    //Last request to keep on choosing songs
+    let lastRequest = '';
 
+    function init() {
+
+        //Message if there is no current search
+        if ($('.search-results').text().length === 0) {
+            $('.search-results').append(`<h1>Aucune recherche actuellement</h1>`);
+        }
+
+        //Get the last request url if existant
+        if(sessionStorage[SESSIONSTORAGE_ID]) {
+            lastRequest = sessionStorage.getItem([SESSIONSTORAGE_ID]);
+            search(lastRequest);
+        }
+
+        //Check localstorage to fill the fav array
+        if (localStorage[LOCALSTORAGE_ID]) {
+            favorites = JSON.parse(localStorage.getItem([LOCALSTORAGE_ID]));
+
+            //Empty the ctn of your fav songs
             $('.favorites-ctn').empty();
 
+            //Diplay alll your fav songs
             for (let i = 0; i < favorites.length; i++) {
                 const song = favorites[i];
                 $('.favorites-ctn').append(
@@ -27,47 +51,53 @@
                         </div>
                     </div>
                 `);
+
+                //Event when you want to delete a fav song
                 $(`.delete #${song.id}`).click(function (event) {
                     deletefavorite(i, song);
                 })
             }
         }
 
+        //Event when you click to search a song
         startSearch.click(function (event) {
             event.preventDefault();
+            const searchValue = $('#search').val();
+            const sortValue = $('#sort').val();
+            const url = `https://api.deezer.com/search?q=${searchValue}&order=${sortValue}&output=jsonp`;
             $('.search-results').empty();
-            search();
+            search(url);
         });
 
-        if ($('.search-results').text().length === 0) {
-            $('.search-results').append(`<h1>Aucune recherche actuellement</h1>`);
-        }
-
+        //Choose a random fav song
         randomFavoriteSong(favorites);
 
+        //Message if you have no fav songs
         if (favorites.length <= 0) {
             $('.favorites-ctn').append(`<h1>Aucun Favoris dans votre playlist actuellement...</h1>`);
         }
     }
 
-    function search() {
-        const searchValue = $('#search').val();
-        const sortValue = $('#sort').val();
+    //Function to launch the search
+    function search(requestUrl) {
         $.ajax({
-            url: `https://api.deezer.com/search?q=${searchValue}&order=${sortValue}&output=jsonp`,
+            url: requestUrl,
             dataType: 'jsonp',
             error: function(request, status, error) {
                 console.log(request, status, error);
             }
-        })
+        })        
             .then(obj => {
                 const songs = obj.data;
+                lastRequest = requestUrl;
+                sessionStorage.setItem(SESSIONSTORAGE_ID, lastRequest);
 
                 if (songs === undefined || songs.length <= 0) {
                     $('.search-results').append(`
                     <h1>Ooups, on dirait qu'il n'y ait pas de résultat pour cette recherche ...</h1>
                     `);
                 } else {
+                    $('.search-results').empty();
                     $.each(songs, function (index, song) {
                         if (isAlreadyAdded(song, favorites) !== true) {
                             $('.search-results').append(
@@ -105,9 +135,10 @@
                                     </div>
                                 </div>
                             `);
-
+                            //Index of the song to delete
                             let favIndex = getIndex(song, favorites);
-
+                            
+                            //Deletion of the fav song
                             $(`.add-song-${song.id} #add-${song.id}`).click(function (event) {
                                 deletefavorite(favIndex, song);
                                 onChangeDelete(song, favIndex);
@@ -116,6 +147,7 @@
                     });
                 }
             })
+            //Errors
             .catch(error => {
                 if (error) {
                     console.error(error.message);
@@ -124,6 +156,7 @@
             })
     }
 
+    //Put the song in your favorites
     function addFavorite(song, index) {
         favorites.push(song);
         localStorage.setItem(LOCALSTORAGE_ID, JSON.stringify(favorites));
@@ -138,6 +171,7 @@
         });
     }
 
+    //Delete the specific fav song 
     function deletefavorite(index, song) {
         console.log(index);
         favorites.splice(index, 1);
@@ -146,6 +180,7 @@
         init();
     }
 
+    //Changing of the button when you need to delete one of your fav songs
     function onChangeDelete(song, index) {
         $(`.add-song-${song.id}`).empty();
         $(`.add-song-${song.id}`).append(`
@@ -158,9 +193,12 @@
         });
     }
 
+    //Diplay a random of your fav songs
     function randomFavoriteSong(favorites) {
         $('.random-favorite').empty();
+        //Check your fav array to display a random song
         if (favorites.length > 0) {
+            //Random song to display
             let randomSong = favorites[Math.floor(Math.random() * favorites.length)];
             $('.random-favorite').append(
                 `
@@ -176,16 +214,18 @@
                     </div>
                 </div>
             `);
-
+            //Display another random song
             $(`#random-${randomSong.id}`).click(function (event) {
                 randomFavoriteSong(favorites);
             });
         } else {
+            //If there is no fav song
             $('.random-favorite').append(
                 `<h1>Enregistrez votre premier coup de coeur pour voir un titre aléatoire !</h1>`);
         }
     }
 
+    //Return if the song is already added
     function isAlreadyAdded(song, favorites) {
         for (let i = 0; i < favorites.length; i++) {
             if (favorites[i].id === song.id) {
@@ -194,6 +234,7 @@
         }
     }
 
+    //Get index of the fav array to delete it
     function getIndex(song) {
         for (let i = 0; i < favorites.length; i++) {
             if (favorites[i].id === song.id) {
@@ -202,6 +243,7 @@
         }
     }
 
+    //App init
     init();
 
 })();
